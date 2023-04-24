3ea2752f2dd3042b7782f726735bbe794f0006d2
maregrizzle <maregrizzle@gmail.com>
2023-04-24 04:41:03 -0400

    trying to fix css

--- louies_garden_project/production.py
+++ louies_garden_project/production.py
@@ -8,9 +8,21 @@ ALLOWED_HOSTS = ['https://louies-garden.onrender.com/','louies-garden.onrender.c
 # Use the environment variable for the secret key
 SECRET_KEY = os.environ['SECRET_KEY']
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Configure static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# production.py
+MIDDLEWARE = [
+    # ...
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ...
+]
+
+# Add this line to enable GZip compression for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Add any other production-specific settings here
