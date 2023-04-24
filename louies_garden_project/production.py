8a05a8869430236f3073368b2970a6d8bd625e6f
maregrizzle <maregrizzle@gmail.com>
2023-04-24 04:09:31 -0400

    fix build

---
+++ louies_garden_project/production.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['louies-garden.onrender.com', 'louiesgarden.com', 'www.louiesgarden.com']
+
+
+# Use the environment variable for the secret key
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+# Configure static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Add any other production-specific settings here
